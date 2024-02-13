5d3cac82a43281c8c25651642eda806549085aa5
Dexametasona <machicadogomezalexander@gmail.com>
2024-02-13 08:48:35 -0500

    feat:adding lazy loading

--- frontend/src/app/modules/layout/layout-routing.module.ts
+++ frontend/src/app/modules/layout/layout-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:LayoutComponent, children:[
+    {path:"" , redirectTo:"/home", pathMatch:"full"},
+    {path:"login", loadChildren:()=>import("../auth/pages/login/login.module").then(m=>m.LoginModule)},
+    {path:"register", loadChildren:()=>import("../auth/pages/register/register.module").then(m=>m.RegisterModule)},
+    {path:"home", loadChildren:()=>import("../home/home.module").then(m=>m.HomeModule)}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
