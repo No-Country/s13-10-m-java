0e9ee45b9d25d898e220478d9946c81fca496b61
Federico Acosta <fede.acosta2396@gmail.com>
2024-02-15 11:22:53 -0300

    se arreglo el mapper

--- ecoapp/src/main/java/com/nocountrys13/ecoapp/dtos/PuntoVerdeDto.java
+++
@@ -1,6 +0,0 @@
-package com.nocountrys13.ecoapp.dtos;
-
-import com.nocountrys13.ecoapp.entities.Usuario;
-
-public record PuntoVerdeDto(String nombre, String direccion, Usuario usuario) {
-}
---
+++ ecoapp/src/main/java/com/nocountrys13/ecoapp/dtos/response/PuntoVerdeDto.java
@@ -0,0 +1,6 @@
+package com.nocountrys13.ecoapp.dtos.response;
+
+import com.nocountrys13.ecoapp.entities.Usuario;
+
+public record PuntoVerdeDto(String nombre, String direccion) {
+}
