906c08051cbba4903bb18f6d0a7b30c2ea2731cd
Federico Di Cillo <dicillo.fe@gmail.com>
2024-02-15 13:34:04 -0300

    add: children routes to Auth-routing

--- frontend/src/app/modules/auth/auth-routing.module.ts
+++ frontend/src/app/modules/auth/auth-routing.module.ts
@@ -5,9 +5,11 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  {path:"", component:AuthComponent},
+  {path:"", component:AuthComponent, children:[
     {path:"login", component:LoginComponent},
     {path:"register", component:RegisterComponent}
+  ]
+}
 ];
 
 @NgModule({
