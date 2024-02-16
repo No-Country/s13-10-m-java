9ae123aae4a07de294d7157929ba3293ff4b8c16
Federico Acosta <fede.acosta2396@gmail.com>
2024-02-16 09:58:39 -0300

    se agrego endpoint para modificar punto verde

--- ecoapp/src/main/java/com/nocountrys13/ecoapp/dtos/response/PuntoVerdeDto.java
+++ ecoapp/src/main/java/com/nocountrys13/ecoapp/dtos/response/PuntoVerdeDto.java
@@ -1,6 +1,7 @@
 package com.nocountrys13.ecoapp.dtos.response;
 
 import com.nocountrys13.ecoapp.entities.Usuario;
+import jakarta.validation.constraints.NotBlank;
 
-public record PuntoVerdeDto(String nombre, String direccion) {
+public record PuntoVerdeDto(@NotBlank String nombre, @NotBlank String direccion) {
 }
