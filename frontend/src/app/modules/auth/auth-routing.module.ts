78cb9a62622954097a76cec37dafe836f5d8a39c
Federico Di Cillo <dicillo.fe@gmail.com>
2024-02-15 22:01:03 -0300

    setting default route for auth

--- frontend/src/app/modules/auth/auth-routing.module.ts
+++ frontend/src/app/modules/auth/auth-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   {path:"", component:AuthComponent, children:[
+    {path:"" , redirectTo:"/auth/login", pathMatch:"full"},
     {path:"login", component:LoginComponent},
     {path:"register", component:RegisterComponent}
   ]
