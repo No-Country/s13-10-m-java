a440c11a76e586babba9401fc76d7780a8dbf9ff
Pablo Aquino <pabloaquino1988@gmail.com>
2024-02-23 14:08:49 -0300

    OpenAPI30Configuration  nueva configuración de swagger

--- ecoapp/src/main/java/com/nocountrys13/ecoapp/config/OpenAPI30Configuration.java
+++ ecoapp/src/main/java/com/nocountrys13/ecoapp/config/OpenAPI30Configuration.java
@@ -1,14 +1,30 @@
 package com.nocountrys13.ecoapp.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@SecurityScheme(
-        name = "Bearer Authentication",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-) public class OpenAPI30Configuration {
+public class OpenAPI30Configuration {
+    private static final String API_KEY = "apiKey";
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(API_KEY, apiKeySecuritySchema()))
+                .info(new Info().title("API - GreenPoint").description(
+                        "RESTful services documentation with OpenAPI 3."))
+                .addSecurityItem(new SecurityRequirement().addList(API_KEY));
+    }
+    public SecurityScheme apiKeySecuritySchema() {
+        return new SecurityScheme()
+                .name("authorization")
+                .description("Description about the TOKEN")
+                .in(SecurityScheme.In.HEADER)
+                .type(SecurityScheme.Type.APIKEY);
+    }
 }
