bfd248842dfb2650925e93307015856eae9b0040
Alben Bustamante <albenbustamante@gmail.com>
2024-02-23 22:11:35 -0500

    update: home button

--- frontend/src/app/modules/home/home.component.ts
+++ frontend/src/app/modules/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   isMenuOpen: boolean = false;
@@ -13,8 +14,12 @@ export class HomeComponent {
   }
   isLogged = false;
 
-  constructor() {
+  constructor(private readonly router: Router) {
     // Verifica si hay un token presente en el localStorage al inicializar el componente
     this.isLogged = !!localStorage.getItem('token');
+  }
+
+  goToRegister() {
+    this.router.navigateByUrl('/auth/register');
   }
 }
