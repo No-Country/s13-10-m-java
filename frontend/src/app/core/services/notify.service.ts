32e76371a5c1a7f39fce8e9d65a4fc2d5d0cab5e
CyraxDev <157859671+CyraxDev@users.noreply.github.com>
2024-02-27 09:00:30 -0500

    update changes

--- frontend/src/app/core/services/notify.service.ts
+++ frontend/src/app/core/services/notify.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NotifyService {
-  /* constructor(private matsnack: MatSnackBar) {}
+  /*constructor(private matsnack: MatSnackBar) {}
 
   showSuccess(message: string, title: string) {
     this.matsnack.open(message, title);
--- frontend/src/app/modules/auth/pages/login/login.component.ts
+++ frontend/src/app/modules/auth/pages/login/login.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { LoginService } from '@services/login.service';
 import { Router } from '@angular/router';
-import { Login } from '@models/login.model';
 
 import { NotifyService } from '@services/notify.service';
 import { emailValidator } from '@utils/validator';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -15,6 +15,7 @@ import { emailValidator } from '@utils/validator';
 export class LoginComponent {
   loginForm: FormGroup;
   showPassword: boolean = false;
+  error: string = 'Error en la aplicacion';
 
   constructor(
     private loginService: LoginService,
@@ -39,6 +40,7 @@ export class LoginComponent {
     if (this.loginForm.invalid) {
       return this.loginForm.markAllAsTouched();
     }
+
     console.log(this.loginForm.value);
 
     this.loginService.Login(this.loginForm.value).subscribe((res: any) => {
@@ -46,48 +48,30 @@ export class LoginComponent {
       this.loginService.token = res.token;
       localStorage.setItem('id', res.id);
       localStorage.setItem('token', res.token);
-      this.router.navigate(['/home']);
     });
-  }
 
-  register() {
-    this.router.navigate(['/home']);
-  }
+    console.log(this.loginForm.markAsTouched);
 
-  onSubmit() {
-    if (this.loginForm.invalid) {
-      // Realizar la lógica de inicio de sesión
-      /* this.notifySvc.showError(
-        'Error al iniciar sesión',
-        'Error con tus datos'
-      ); */
-
-      return;
-    }
-
-    const user: Login = {
-      email: this.loginForm.value.email,
-      password: this.loginForm.value.password,
-    };
-    /* this.loginService.Login(user).subscribe({
+    this.loginService.Login(this.loginForm.value).subscribe({
       next: () => {
-        this.notifySvc.(
-          'Inicio de sesión exitoso',
-          'Bienvenido a EcoApp'
-        );
-        this.router.navigate(['']);
-        setTimeout(() => {
-          location.href = '/home';
-        }, 3000);
+        Swal.fire({
+          title: '¡Ingreso exitoso!',
+          text: `¡Hola, ${this.loginService.Login}! Seras redirigido a nuestro inicio.`,
+          icon: 'success',
+        }).then(() => this.loginForm.reset());
       },
-      error: (err) => {
-        this.notifySvc.showError(
-          'Error al iniciar sesión',
-          'Error con tus datos'
-        );
+      error: (error) => {
+        Swal.fire({
+          title: 'Ha ocurrido un error...',
+          text: error.error,
+          icon: 'error',
+        });
       },
-    }); */
+    });
+
+    return;
   }
+
   handleShowPassword(): void {
     this.showPassword = !this.showPassword;
   }
