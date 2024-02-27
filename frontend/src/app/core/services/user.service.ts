33fea7784f37850ac3153ce6e43c99fa0cec0d10
Dexametasona <machicadogomezalexander@gmail.com>
2024-02-27 18:34:41 -0500

    feat:adding register greenpoint modal

---
+++ frontend/src/app/core/services/user.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '@environments/environment';
+import { UserResponse } from '@models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  private readonly URL = environment.apiUrl+"/usuario"
+  constructor(private http:HttpClient) { }
+
+  getUser(id:string){
+    return this.http.get<UserResponse>(this.URL+`/${id}`)
+  }
+}
--- frontend/src/app/modules/dashboard/components/header/header.component.ts
+++ frontend/src/app/modules/dashboard/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { tokenData } from '@models/token.model';
+import { TokenService } from '@services/token.service';
 
 
 @Component({
@@ -9,8 +11,22 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class HeaderComponent {
 
   @Output() emitShowSidebar = new  EventEmitter<boolean>();
+  @Output() emitShowModal = new  EventEmitter<boolean>();
+  tokenData !: tokenData | undefined;
+
+  constructor(
+    private tokenService:TokenService
+  ){}
+
+  ngOnInit(){
+    this.tokenData = this.tokenService.getTokenDecoded()
+  }
   showSidebar(){
     this.emitShowSidebar.emit(true);
+  }
+
+  showModal(){
+    this.emitShowModal.emit(true);
   }
   showOptions(){
    var boton= document.getElementById("dropdownDivider");
