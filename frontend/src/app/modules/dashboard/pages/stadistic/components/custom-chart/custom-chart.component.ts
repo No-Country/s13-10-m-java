26581aee7dc7fbddb781e474da82d0bd6bc1efbe
Dexametasona <machicadogomezalexander@gmail.com>
2024-02-28 22:43:52 -0500

    feat:adding template and styles to stadistic view

--- frontend/src/app/modules/dashboard/pages/stadistic/components/custom-chart/custom-chart.component.ts
+++ frontend/src/app/modules/dashboard/pages/stadistic/components/custom-chart/custom-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartData, plugins } from 'chart.js';
 
 @Component({
   selector: 'app-custom-chart',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./custom-chart.component.scss'],
 })
 export class CustomChartComponent {
-
+  chartLabels = ['Sales'];
+  chartOptions = {
+    responsive: true,
+  }
+  datasets: ChartData <'line', {key: string, value: number} []> = {
+    datasets: [{
+      data: [
+        {key: 'sem 1', value: 20},
+        {key: 'sem 2 ', value: 10},
+        {key: 'sem 3 ', value: 30},
+        {key: 'sem 4 ', value: 40},
+        {key: 'sem 5 ', value: 5},
+      ],
+      parsing: {
+        xAxisKey: 'key',
+        yAxisKey: 'value'
+      },
+      backgroundColor:"#FE6283",
+      tension:0.5,
+      label:"Greenpoints",
+    }],
+  };
 }
--- frontend/src/app/modules/dashboard/pages/stadistic/stadistic.module.ts
+++ frontend/src/app/modules/dashboard/pages/stadistic/stadistic.module.ts
@@ -5,17 +5,19 @@ import { StadisticRoutingModule } from './stadistic-routing.module';
 import { StadisticComponent } from './stadistic.component';
 import { ScoreComponent } from './components/score/score.component';
 import { CustomChartComponent } from './components/custom-chart/custom-chart.component';
+import { NgChartsModule } from 'ng2-charts';
 
 
 @NgModule({
   declarations: [
     StadisticComponent,
     ScoreComponent,
-    CustomChartComponent
+    CustomChartComponent,
   ],
   imports: [
     CommonModule,
     StadisticRoutingModule,
+    NgChartsModule
   ]
 })
 export class StadisticModule { }
