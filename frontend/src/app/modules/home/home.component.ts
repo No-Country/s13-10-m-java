731c9c435124b4aae62945df24dd2f86d509a673
Alben Bustamante <albenbustamante@gmail.com>
2024-03-01 10:52:19 -0500

    remove: unnecessary request on home page

--- frontend/src/app/modules/home/home.component.ts
+++ frontend/src/app/modules/home/home.component.ts
@@ -1,22 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
-  constructor(
-    private readonly router: Router,
-    private readonly authService: AuthService
-  ) {
+export class HomeComponent {
+  constructor(private readonly router: Router) {
     this.isLogged = !!localStorage.getItem('token');
-  }
-
-  ngOnInit(): void {
-    this.authService.getUserLogged().subscribe();
   }
   isMenuOpen: boolean = false;
 
