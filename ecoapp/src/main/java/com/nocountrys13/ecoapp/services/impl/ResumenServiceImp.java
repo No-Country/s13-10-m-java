82a069edd74ebe9feca1302769b7fbefac66c7a7
leonardofvp <leonardovargasfp@gmail.com>
2024-03-02 16:16:31 -0300

    se finalizo el service de resumen, falta implementar metodos que devuelven resumen de usuario

--- ecoapp/src/main/java/com/nocountrys13/ecoapp/services/impl/ResumenServiceImp.java
+++ ecoapp/src/main/java/com/nocountrys13/ecoapp/services/impl/ResumenServiceImp.java
@@ -1,5 +1,8 @@
 package com.nocountrys13.ecoapp.services.impl;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -8,47 +11,51 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.nocountrys13.ecoapp.dtos.response.ResumenPuntoVerdeResponse;
 import com.nocountrys13.ecoapp.dtos.response.ResumenUsuarioResponse;
+import com.nocountrys13.ecoapp.entities.PuntoVerde;
+import com.nocountrys13.ecoapp.entities.Usuario;
+import com.nocountrys13.ecoapp.repositories.UsuarioRepository;
 import com.nocountrys13.ecoapp.services.IResumenService;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class ResumenServiceImp implements IResumenService {
+	
+	private final UsuarioRepository usuarioRepository;
+	
+	
 	@Override
-
 	public Object ResumenUser(UserDetails userLogueado) {
-
-		if (usuarioRol(userLogueado, "ROLE_USER") && userLogueado != null) {
-			return resumenUserRole();
+		
+		Optional <Usuario> user = Optional.of( usuarioRepository.findByEmail(userLogueado.getUsername()));
+									
+		if(user.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Noexiste el usuario");
+		}
+		
+		List<PuntoVerde> listaPV = user.get().getPuntosVerdes();
+		
+		if ( listaPV.isEmpty()) {
+			return resumenUserRole(user.get());
 		} else {
-			throw new ResponseStatusException(HttpStatus.BAD_REQUEST, " Error en el token");
+			return resumenUserPuntoVErde(listaPV );
 
 		}
-
-		//return resumenUserPuntoVErde();
 
 	}
 
 	
 	
-	
-	
 	// estadisticas del usuario comun(registrado)
-	private ResumenUsuarioResponse resumenUserRole() {
+	private ResumenUsuarioResponse resumenUserRole(Usuario user) {
 		ResumenUsuarioResponse userResponse = new ResumenUsuarioResponse();
 		return userResponse;
 	}
 
 	// estadisticas del usuario con punto verde
-	private ResumenPuntoVerdeResponse resumenUserPuntoVErde() {
+	private ResumenPuntoVerdeResponse resumenUserPuntoVErde(List<PuntoVerde> listaPV) {
 		ResumenPuntoVerdeResponse userConPuntoVerd = new ResumenPuntoVerdeResponse();
 		return userConPuntoVerd;
-	}
-
-	// verifico el rol del usuario
-	private boolean usuarioRol(UserDetails user, String rolBuscado) {
-		return user.getAuthorities().stream().map(GrantedAuthority::getAuthority)
-				.anyMatch(rol -> rol.equals(rolBuscado));
 	}
 
 }
