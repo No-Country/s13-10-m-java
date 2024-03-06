f95e29cefe1e30a04b1d49b480ffb27cf1522036
David Reyes <reyisacc@gmail.com>
2024-03-06 13:39:52 -0400

    retoques de estilos

--- frontend/src/app/core/pipes/type-format.pipe.ts
+++ frontend/src/app/core/pipes/type-format.pipe.ts
@@ -8,7 +8,7 @@ export class TypeFormatPipe implements PipeTransform {
 
   transform(value: string): string {
     if(value === "PLASTICO") return "Plástico";
-    if(value === "PAPELCARTON") return "Papel y carton";
+    if(value === "PAPELCARTON") return "Papel y cartón";
     if(value === "VIDRIO") return "Vidrio"
     if(value === "METAL") return "Metal"
     return value;
--- frontend/src/app/modules/home/home.component.ts
+++ frontend/src/app/modules/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '@services/auth.service';
 import { UserResponse } from '@models/user.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,7 @@ import { UserResponse } from '@models/user.model';
 })
 export class HomeComponent implements OnInit {
   isLogged = false;
+  imagen = "assets/img/user-icon.svg";
   isMenuOpen: boolean = false;
   UserResponse: UserResponse | null = null;
 
@@ -22,7 +24,10 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     // window.addEventListener('storage', this.handleStorageChange);
-    this.authService.userLogged$.subscribe((res) => {this.UserResponse = res; console.log(res)});
+    this.authService.userLogged$.subscribe((res) => {
+       this.UserResponse = res;
+        console.log(res); 
+    });
   }
 
   toggleMenu() {
@@ -38,5 +43,25 @@ export class HomeComponent implements OnInit {
     if (event.key === 'token') {
       window.location.reload();
     }
+  }
+  showOptions() {
+    var boton = document.getElementById('dropdownDivider');
+    boton?.classList.toggle('hidden');
+  }
+  onLogout() {
+    Swal.fire({
+      title: 'Deseas cerrar sesion?',
+      text: 'Seras dirigido a nuestro inicio de sesion!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, Quiero salir!',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.authService.logout();
+      }
+    });
   }
 }
