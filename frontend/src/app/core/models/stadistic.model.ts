27979092ae73c298b6d6a3a8bf5ece821edc1afb
Dexametasona <machicadogomezalexander@gmail.com>
2024-03-05 22:19:29 -0500

    fix:fixing cards in stadistics

--- frontend/src/app/core/models/stadistic.model.ts
+++ frontend/src/app/core/models/stadistic.model.ts
@@ -1,7 +1,9 @@
 export interface stadisticResponse{
   nombreCompleto: string;
   emailVerificado: boolean;
-  CantPuntosVerde: number;
-  materialesAceptados: string[];
-  cantPremio: number;
+  CantPuntosVerde?: number;
+  materialesAceptados?: string[];
+  cantPremio?: number;
+  puntos?:number;
+  premiosObtenidos?:number;
 }
